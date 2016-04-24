57c06167d000d1760a9fbf5c1e61b524b3152361
Tim Clemans <tim@insideyourgovernment.com>
2016-04-24 02:44:13 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+    app = express();
+
+var oauth2 = require('simple-oauth2')({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  site: 'https://opendata.socrata.com',
+  tokenPath: '/oauth/access_token',
+  authorizationPath: '/oauth/authorize'
+});
+
+// Authorization uri definition
+var authorization_uri = oauth2.authCode.authorizeURL({
+  redirect_uri: 'http://localhost:3000/callback',
+  scope: 'notifications',
+  state: '3(#0/!~'
+});
+
+// Initial page redirecting to Github
+app.get('/auth', function (req, res) {
+    res.redirect(authorization_uri);
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/callback', function (req, res) {
+  var code = req.query.code;
+
+  oauth2.authCode.getToken({
+    code: code,
+    redirect_uri: 'http://localhost:3000/callback'
+  }, saveToken);
+
+  function saveToken(error, result) {
+    if (error) { console.log('Access Token Error', error.message); }
+    token = oauth2.accessToken.create(result);
+  }
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello<br><a href="/auth">Log in with Github</a>');
+});
+
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000);
+
+console.log('Express server started on port 3000');
